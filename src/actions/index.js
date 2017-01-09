fce78097d70970de6c08d5d2f035bd796c591034
lgants <lgants@gmail.com>
2017-01-08 19:15:12 -0600

    completed project

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const FETCH_POST = 'FETCH_POST';
 export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=lkajdsfapipwietpw';
+const API_KEY = '?key=1234asdf';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
